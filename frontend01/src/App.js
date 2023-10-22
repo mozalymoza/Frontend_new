8fcff6031472176ad3571299123d342dcdfed8ff
mozalymoza <mozalymoza@example.com>
2023-10-22 19:56:03 +0700

    modul 19,20

---
+++ frontend01/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Routing from './template/Routing';
+
+function App() {
+  return (
+   <div>
+   <Routing/>
+    </div>
+  );
+}
+
+export default App;
